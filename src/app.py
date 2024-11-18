ec29345e3f7e3fdbab78da7fea93bedca06f6dca
kuosaton <tony.kuosa@outlook.com>
2024-11-18 16:30:00 +0200

    Cleanup boilerplate stuff

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 from flask import redirect, render_template, request, jsonify, flash, url_for
 from db_helper import reset_db
-from repositories.todo_repository import get_todos, create_todo, set_done
 from config import app, test_env
-from util import validate_todo
 from forms import AddArticleForm
 
 from services.article_service import validate_article, UserInputError
@@ -54,27 +52,6 @@ def add_article():
 
     # Render the form again (with error messages if any)
     return render_template("article.html", form=form) 
-
-@app.route("/new_todo")
-def new():
-    return render_template("new_todo.html")
-
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
-    content = request.form.get("content")
-
-    try:
-        validate_todo(content)
-        create_todo(content)
-        return redirect("/")
-    except Exception as error:
-        flash(str(error))
-        return  redirect("/new_todo")
-
-@app.route("/toggle_todo/<todo_id>", methods=["POST"])
-def toggle_todo(todo_id):
-    set_done(todo_id)
-    return redirect("/")
 
 # testausta varten oleva reitti
 if test_env:
