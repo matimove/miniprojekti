c8893bb6db31ca67b8d55b3d0f7b3a2b1debe082
simo <sipilasimo@gmail.com>
2024-11-30 15:02:41 +0200

    add reference service unittest base and test for sorting by title

---
+++ src/tests/test_reference_service_sorting.py
@@ -0,0 +1,39 @@
+import unittest
+
+from services.reference_service import ReferenceService
+
+
+class MockReference:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __eq__(self, value: object):
+        if isinstance(value, MockReference):
+            return self.title == value.title and self.author == value.author and self.year == value.year
+        return False
+
+
+class TestReferenceService(unittest.TestCase):
+    def setUp(self):
+        self.reference_service = ReferenceService()
+        self.reference_service.references = [
+            MockReference("Classical mechanics", "Leonard Susskind", 2014),
+            MockReference("An Analysis of Example", "John Smith and Jane Doe", 2022),
+            MockReference("An Analysis of Example", "xyz", 2022),
+            MockReference("Toimitusketjujen optimointi", "Joulupukki", 2023),
+            MockReference("Aamunsarastus", "Joulupukki", 2023),
+        ]
+
+    def test_sort_references_by_title(self):
+        result = self.reference_service.sort_references_by_title()
+        expected = [
+            MockReference("Aamunsarastus", "Joulupukki", 2023),
+            MockReference("An Analysis of Example", "John Smith and Jane Doe", 2022),
+            MockReference("An Analysis of Example", "xyz", 2022),
+            MockReference("Classical mechanics", "Leonard Susskind", 2014),
+            MockReference("Toimitusketjujen optimointi", "Joulupukki", 2023),
+        ]
+
+        self.assertEqual(result, expected)
