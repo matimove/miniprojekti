3da8345408554d8a88b9618fed90320f7f9287c1
Nanna Ketola <nannailona@gmail.com>
2024-12-08 15:22:19 +0200

    Implemented ability for users to add references with DOI
    - Currently supports reference types "article" and "book"

--- src/repositories/doi_repository.py
+++ src/repositories/doi_repository.py
@@ -1,5 +1,6 @@
 import urllib.request
 from urllib.error import HTTPError
+import bibtexparser
 from validators import validate_doi
 from services.citation_service import UserInputError
 
@@ -20,7 +21,13 @@ def get_bibtex_with_doi(doi):
     try:
         with urllib.request.urlopen(req, timeout=10) as f:
             bibtex = f.read().decode()
-        return bibtex
+
+        bib_database = bibtexparser.loads(bibtex)
+        # Extract the entry
+        entry = bib_database.entries[0]
+        print(entry)
+        return entry
+
     except HTTPError as e:
         if e.code == 404:
             raise "DOI not found."
